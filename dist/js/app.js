891e62a2b1f9057190ed3930ba7a38a5ba2f682f
Denis Berezin <nocab@yandex.ru>
2019-05-16 23:24:59 +0300

    lvl 3.1

---
+++ dist/js/app.js
@@ -0,0 +1,10 @@
+const a = params => {
+  const {
+    str
+  } = params;
+  console.log(str);
+};
+
+a({
+  str: 'Some string'
+});
---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+const a = (params) => {
+	const { str } = params;
+	console.log(str);
+}
+a({ str: 'Some string'});
